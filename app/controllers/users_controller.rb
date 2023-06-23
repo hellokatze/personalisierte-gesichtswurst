d5da5d0b2d2c1d2410f3f5feaddaf875cc86a610
Katharine Lin <katharine.exe@gmail.com>
2023-06-23 14:52:50 +0200

    add users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
